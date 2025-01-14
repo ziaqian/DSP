d2e64106a3183bcae7a8f14ef270f38a89f41dd3
ziaqian <128562280+ziaqian@users.noreply.github.com>
2025-01-14 22:29:15 +0800

    Update evcdp.py

--- evcdp_pages/evcdp.py
+++ evcdp_pages/evcdp.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 @st.cache_resource
 def load_best_model():
     # Explicitly map the 'mse' loss function
-    return load_model("cnnlstm_ev_model.h5", custom_objects={"MeanSquaredError": MeanSquaredError(), "mse": mse_loss()})
+    return load_model("cnnlstm_ev_model.h5", custom_objects={"MeanSquaredError": MeanSquaredError()})
     # return load_model("cnnlstm_ev_model.h5", custom_objects={"mse": MeanSquaredError()})
 
 # Main function for the EVCDP page
